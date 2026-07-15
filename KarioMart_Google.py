6fc290bbc1f29913197270492ee8df0b871b76a0
mkay-cbw <markus.widuch@googlemail.com>
2026-07-15 10:27:38 +0200

    Update KarioMart_Google.py

--- KarioMart_Google.py
+++ KarioMart_Google.py
@@ -103,12 +103,11 @@ def lade_aus_cloud(force=False):
 
         # Lade Tabellen aus Google Sheet und überschreibe lokale DB
         for tabelle in ALL_TABLES:
-            try:
-                df_sheet = sheets_conn.read(worksheet=tabelle, ttl=0)
-                if df_sheet is not None and not df_sheet.empty:
-                    df_sheet.to_sql(tabelle, conn, if_exists="append", index=False)
-            except Exception as e:
-                pass
+            # try:
+            df_sheet = sheets_conn.read(worksheet=tabelle, ttl=0)
+            if df_sheet is not None and not df_sheet.empty:
+                df_sheet.to_sql(tabelle, conn, if_exists="append", index=False)
+            # except Exception as e:
                 # st.error(f"❌ Fehler beim Laden der Tabelle '{tabelle}'! Versuche es später erneut.\n\n{e}")
                 # st.stop()  # Abbruch
 
